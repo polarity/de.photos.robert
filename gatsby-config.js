d478a11e2a4f3cb6a055dd171bfa2b80b18fc4e7
Robert Agthe <robert@scriptshit.de>
2020-01-23 02:23:46 +0100

    changed vars becaused reserved on netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,11 +13,11 @@ module.exports = {
       resolve: '@robinmetral/gatsby-source-s3',
       options: {
         aws: {
-          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-          region: process.env.AWS_REGION
+          accessKeyId: process.env.ACCESS_KEY_ID,
+          secretAccessKey: process.env.SECRET_ACCESS_KEY,
+          region: process.env.REGION
         },
-        buckets: [process.env.AWS_BUCKET]
+        buckets: [process.env.BUCKET]
       }
     },
     'gatsby-plugin-react-helmet',
